eedca6ebf9a8bdedd01c90fe23a46e7db1f2d250
leetlee <leetxyli@gmail.com>
2015-04-13 09:54:24 +0800

    PHP Parser Version 1
    Re-implement the parser.py in PHP
    0. Using a external lib: PHP Markdown in folder Michelf
    1. Define a PHP class Parser in parser.inc, similar structure of
    parser.py
    2. Reorganise the config file into config
    3. Rename Keys used in $_POST of index.php, also related parts in
    template.js
    
    — Todo—
    Implements the functions of Class Parser

---
+++ presentPHP/API/mdSrc/Slides.md
@@ -0,0 +1,63 @@
+Towards Better Computer-Based Presentations
+===============
+
+Fixed Size
+========
+Limited edit space -- Slide-based
+Authors have to edit the contents to fit on a slide or slides.
+
+
+Shortcomings of slideware
+============
+
+##Limited Space ##
+
+Slide-based:
+
+Authors have to edit the contents to fit on a slide or slides.
+
+##Strictly Sequentiality##
+
+1. The presenter may want to present in different sequence from the predefined one
+  - because present occasions are different
+  - because available time is different
+  - because some impromptu situation
+
+2. There is Only one predefined sequence in PPT and Prezi
+
+3. No support for a Plan B / Game Plan / Dynamic Sequence
+
+4. E.G. : The presenter may prepare some spare slides for explaining a topic more
+- If someone asks related questions, the presenter will show the slides
+- If no one ask, the presenter does not need to show the slides
+
+##Inevitable Linearity##
+Only suitable for Hierarchical Structure, e.g linear or tree structure : 
+
+NOT suitable for Complex Structure, e.g. graph
+
+Current solution is using Interlink to Go Back or Duplicate when defining the sequence
+
+Prezi and other ZUI tools provide an Unlimited Zoomable Canvas
+
+##Fix Style##
+The style must be pre-defined during authoring.
+
+Do NOT support Content Adaptation:
+- The style can NOT be changed while presenting
+- The style can NOT be responsive to different factors (device, screen size)
+
+Related Improvement
+================================
+
+##Approach based on ZUI##
+
+1. Slide-based Plugin
+
+2. Slide-free Paradigm
+- Prezi
+- Fly
+
+##Approach based on different hardware##
+
+multi-preaenter
---
+++ presentPHP/js/starter-template.js
@@ -0,0 +1,86 @@
+window.console = window.console || function(t) {};
+window.open = function(){ console.log('window.open is disabled.'); };
+window.print = function(){ console.log('window.print is disabled.'); };
+// Support hover state for mobile.
+if (false) {
+	window.ontouchstart = function(){};
+}
+
+/* For Bootstrap Initialize */
+// initialize with defaults
+$("#fileInput").fileinput();
+$("#cssInput").fileinput();
+ 
+/*  with plugin options */
+// $("#fileInput").fileinput({'showUpload':false, 'previewFileType':'any'});
+// $("#cssInput").fileinput({'showUpload':false, 'previewFileType':'any'});
+
+/* Display selected md file in left textarea*/
+window.onload = function () {
+	var fileInput = document.getElementById('fileInput');
+	var fileDisplayArea = document.getElementById('inputMD');
+	fileInput.addEventListener('change', function (e) {
+		var file = fileInput.files[0];
+		var textType = /text.*/;
+		if (file.type.match(textType)) {
+			var reader = new FileReader();
+			reader.onload = function (e) {
+				fileDisplayArea.innerText = reader.result;
+			};
+			reader.readAsText(file);
+		} else {
+			fileDisplayArea.innerText = 'File not supported!';
+		}
+	});
+};
+
+function preview(){
+	var src = document.getElementById('present').src;
+	var src_def = "http://127.0.0.1:8000/PresentSystem/"
+	if (src != src_def ){
+		// alert("open "+src+" in a new window");
+		// window.open(src, '_blank');
+		document.getElementById('element_to_pop_up').src=src;
+		$('#element_to_pop_up').bPopup({
+			position: ['auto', 20]
+		});
+	} else {
+		alert("Nothing is ready for preview");
+	}
+
+
+};
+
+/* Actions performed by button Convert*/
+
+function convert()
+{
+	var supported_paradigm =
+	{
+		"S5"	:{"paradigm":"slide" 	, "file_name":"[S5]Markdown Syntax.html",},
+		"Scroll":{"paradigm":"scroll" 	, "file_name":"[scroll]Markdown Syntax.html"},
+		"Slidy"	:{"paradigm":"slide" 	, "file_name":"[slidy]Markdown Syntax.xhtml"},
+	}
+
+	var radios = document.getElementById('paradigm');
+	var paradigm = radios.options[radios.selectedIndex].text;
+
+	var is_support = false
+	for (var key in supported_paradigm)	{
+		if (key == paradigm){
+			is_support = true;
+			break;
+		}
+	}
+
+	if (is_support){
+		
+		f = document.forms['authorInput'];
+		f.elements.namedItem('inputStyle').value = paradigm;
+		f.elements.namedItem('inputSubmit').value = 'true';
+		f.submit();
+	} else {
+		alert("NOT Support Paradigm! "+paradigm);
+	}
+};
+
